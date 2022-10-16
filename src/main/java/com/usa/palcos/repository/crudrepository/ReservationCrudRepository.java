874f2cf6c16598613c4990bc836a4cc3e2d273eb
jhonnatan-Usa <jhonnatan.cifuentes.mt@usa.edu.co>
2022-10-16 18:38:53 -0500

    minimus changes

--- src/main/java/com/usa/palcos/repository/crudrepository/ReservationCrudRepository.java
+++ src/main/java/com/usa/palcos/repository/crudrepository/ReservationCrudRepository.java
@@ -10,9 +10,9 @@ import java.util.List;
 public interface ReservationCrudRepository extends CrudRepository<Reservation, Integer> {
     public List<Reservation> findAllByStatus(String status);
 
-    List<Reservation> findAllByStartDateAfterAndStartDateBefore(Date dateOne, Date dateTwo);
+    public List<Reservation> findAllByStartDateAfterAndStartDateBefore(Date dateOne, Date dateTwo);
 
     @Query("SELECT c.client, COUNT(c.client) FROM Reservation AS c GROUP BY c.client ORDER BY COUNT(c.client) DESC")
-    List<Object[]> countTotalReservationsByClient ();
+    public List<Object[]> countTotalReservationsByClient ();
 
 }
